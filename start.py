c503852b5238c7321080f9123ca3716072371e3a
Lilfjjdb <86081096+Lilfjjdb@users.noreply.github.com>
2021-06-17 20:05:57 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/Lilfjjdb/ultra_x_pro && cd ultra_x_pro && python3 -m ULTRA")
+system ("git clone https://github.com/hhhghbbxbxb/ultra_x_pro && cd ultra_x_pro && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
